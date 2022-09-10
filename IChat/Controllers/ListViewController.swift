846fe465600d8aa9204e25c00568e1a45b5cdcc9
Andriu2431 <andrijmalik2@gmail.com>
2022-09-10 14:18:52 +0300

    Create search bar

--- IChat/Controllers/ListViewController.swift
+++ IChat/Controllers/ListViewController.swift
@@ -10,15 +10,65 @@ import UIKit
 // це екран коли юзер уже увійшов - екран де всі чати покзані
 class ListViewController: UIViewController {
     
+    var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+
+        setupCollectionView()
+        setupSearchBar()
+    }
+    
+    // метод створює search bar
+    private func setupSearchBar() {
+        let searchController = UISearchController(searchResultsController: nil)
+        // в navigationBar вставляємо searchController
+        navigationItem.searchController = searchController
+        // не ховаємо searchController під час скролінгу
+        navigationItem.hidesSearchBarWhenScrolling = false
+        // не приховує панель навігації під час скролінгу
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.delegate = self
+    }
+    
+    // настройка collectionView
+    private func setupCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .mainWhite()
+        view.addSubview(collectionView)
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellid")
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+}
+
+// MARK: UICollectionViewDelegate, UICollectionViewDataSource
+extension ListViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellid", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+}
+
+// MARK: UISearchBarDelegate
+extension ListViewController: UISearchBarDelegate {
+    // коли ми вписуємо щось в searchController
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
     }
 }
 
 
 // MARK: SwiftUI
-
 import SwiftUI
 
 struct ListVCProvider: PreviewProvider {
@@ -28,10 +78,10 @@ struct ListVCProvider: PreviewProvider {
     
     struct ContainerView: UIViewControllerRepresentable {
         
-        let viewController = SetupProfileViewController()
+        let tabBarVC = MainTabBarController()
         
         func makeUIViewController(context: Context) -> some UIViewController {
-            return viewController
+            return tabBarVC
         }
         
         func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
--- IChat/Controllers/PeopleViewController.swift
+++ IChat/Controllers/PeopleViewController.swift
@@ -13,5 +13,50 @@ class PeopleViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        setupSearchBar()
+    }
+    
+    // метод створює search bar
+    private func setupSearchBar() {
+        let searchController = UISearchController(searchResultsController: nil)
+        // в navigationBar вставляємо searchController
+        navigationItem.searchController = searchController
+        // не ховаємо searchController під час скролінгу
+        navigationItem.hidesSearchBarWhenScrolling = false
+        // не приховує панель навігації під час скролінгу
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.delegate = self
+    }
+}
+
+// MARK: UISearchBarDelegate
+extension PeopleViewController: UISearchBarDelegate {
+    // коли ми вписуємо щось в searchController
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
+    }
+}
+
+// MARK: SwiftUI
+
+import SwiftUI
+
+struct PeopleVCProvider: PreviewProvider {
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        
+        let tabBarVC = MainTabBarController()
+        
+        func makeUIViewController(context: Context) -> some UIViewController {
+            return tabBarVC
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+            
+        }
     }
 }
