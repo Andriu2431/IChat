c8983fc19ad634d8e3ed7680173de217b9970e36
Andriu2431 <andrijmalik2@gmail.com>
2022-09-21 14:59:01 +0300

    Fixced bugs

--- IChat/SignIn&Auth/SetupProfileViewController.swift
+++ IChat/SignIn&Auth/SetupProfileViewController.swift
@@ -40,9 +40,9 @@ class SetupProfileViewController: UIViewController {
         }
         
         // тут перевіримо чи є фото в юзера - також буде фото якщо він рейструється через гугл
-//        if let photoURL = currentUser.photoURL {
-//            fullImageView.circleImageView.sd_setImage(with: photoURL)
-//        }
+        if let photoURL = currentUser.photoURL {
+            fullImageView.circleImageView.sd_setImage(with: photoURL)
+        }
     }
         
     override func viewDidLoad() {
